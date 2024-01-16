a7cb8f54e3665025fd215ede7117fe23be294b40
Harsh <79529055+harshverma724@users.noreply.github.com>
2024-01-16 22:31:31 +0530

    Update and rename so.java to Table_Of_A_Number.java
    You can enter any number and can get table of it.

--- Table_Of_A_Number.java
+++ Table_Of_A_Number.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class so {
+public class Table_Of_A_Number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. here: ");
