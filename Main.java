c4f9d2a4abfad5faccb0def088c1a6d15338b7bc
Harsh <79529055+harshverma724@users.noreply.github.com>
2024-02-09 23:14:30 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int a = sc.nextInt();
+
+        if (a % 2 == 0) {
+            System.out.println("Number is Even.");
+
+        } else {
+            System.out.println("Number is Odd.");
+        }
+
+    }
+
+}
