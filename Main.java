5548918b263d85d29a5660299d0ace2600af0893
Harsh <79529055+harshverma724@users.noreply.github.com>
2024-02-09 22:46:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number 1 here: ");
+        int a = sc.nextInt();
+        System.out.print("Enter number 2 here: ");
+        int b = sc.nextInt();
+        System.out.print("Enter number 3 here: ");
+        int c = sc.nextInt();
+
+        if (a > b) {
+            if (a > c) {
+                System.out.println("A is Biggest.");
+            } else {
+                System.out.println("C is Biggest. ");
+            }
+        }
+
+        else if (b > a) {
+            if (b > c) {
+                System.out.println("B is Biggest. ");
+            } else {
+                System.out.println("C is Biggest. ");
+            }
+
+        }
+
+        else {
+            System.out.println("C is Biggest. ");
+        }
+
+    }
+
+}
