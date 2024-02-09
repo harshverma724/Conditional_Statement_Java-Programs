622872dc10dc7c26164b2a8ef836b1f79263de2c
Harsh <79529055+harshverma724@users.noreply.github.com>
2024-02-09 23:37:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int a = sc.nextInt();
+
+        if (a % 5 == 0) {
+            if (a % 11 == 0) {
+                System.out.println("Number is Divisible by 5 and 11.");
+            } else {
+
+                System.out.println("Number is Divisible by 5 but not by 11.");
+
+            }
+        }
+
+        if (a % 11 == 0) {
+
+            System.out.println("Number is Divisible by 11 but not by 5.");
+
+        } else {
+            System.out.println("Number is not Divisible by both 5 and 11.");
+        }
+
+    }
+
+}
